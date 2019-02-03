d1752befb816286ee8dc94310841b3beb3a7f67c
Mihai Raducanu <raducanu@gmail.com>
2019-02-03 19:43:01 +0200

    feat: updated tictactoe

--- src/index.jsx
+++ src/index.jsx
@@ -2,30 +2,41 @@ import React, { Component, Fragment } from 'react'
 import ReactDOM from 'react-dom'
 import { compose } from 'recompose'
 
-const winning = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
-const Token = ({ token, ...props }) => <div {...{ ...props, className: 'token' }}>{token}</div>
+const initialState = { tokens: Array(9).fill(''), turn: 'X', ended: false, winner: null }
 
-const Status = ({ text }) => <div>{text}</div>
+const Token = ({ token, winner, ...props }) => (
+  <div {...{ ...props, disabled: winner || token !== '', className: 'token' }}>{token}</div>
+)
+
+const Status = ({ text }) => <div {...{ className: 'status' }}>{text}</div>
+
+const Reset = props => <button {...{ ...props, className: 'reset' }}>RESET</button>
 
 class Board extends Component {
   constructor(props) {
     super(props)
-    this.state = { tokens: Array(9).fill(''), turn: 'X', ended: false, winner: null }
+    this.state = initialState
     this.place = this.place.bind(this)
+    this.reset = this.reset.bind(this)
   }
 
   place(pos) {
-    const { tokens, turn } = this.state
+    const { tokens, turn, winner } = this.state
 
-    if (tokens[pos] === '') {
+    if (!winner && tokens[pos] === '') {
       const newTokens = tokens.map((_, i) => (i === pos ? turn : _))
       const ended = newTokens.every(token => token !== '')
 
-      const hasWon = winning.some(com => com.every(pos => newTokens[pos] === turn))
+      const hasWon = lines.some(com => com.every(pos => newTokens[pos] === turn))
 
       this.setState({ tokens: newTokens, turn: turn === 'X' ? '0' : 'X', ended, winner: hasWon ? turn : null })
     }
+  }
+
+  reset() {
+    this.setState(initialState)
   }
 
   render() {
@@ -34,10 +45,11 @@ class Board extends Component {
       <Fragment>
         <div className="board">
           {tokens.map((token, key) => (
-            <Token {...{ key, token, onClick: () => this.place(key) }} />
+            <Token {...{ key, token, winner, onClick: () => this.place(key) }} />
           ))}
         </div>
         <Status {...{ text: winner ? `${winner} WON` : ended ? 'ENDED' : '' }} />
+        {winner && <Reset {...{ onClick: this.reset }} />}
       </Fragment>
     )
   }
