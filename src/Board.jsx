40e06fb712155b88d3dd2996391186d43b3b7565
Raducanu <MRaducanu@DBDC.luxoft.com>
2019-02-06 10:33:02 +0200

    chore: Switched to recompose

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-import { compose } from 'recompose'
+import { compose, renderComponent } from 'recompose'
 
 const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
